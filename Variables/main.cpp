9ceef7902284c4f734b35c69ca6bcefb54073d96
MoamenAbouhaty95 <momenabohaty@gmail.com>
2026-01-13 01:52:35 +0200

    Initial commit: add variables folder with basic C++ examples

---
+++ Variables/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // Integer variable
+    int age = 25;
+
+    // Floating point variable
+    float height = 175.5;
+
+    // Double variable
+    double salary = 5500.75;
+
+    // Character variable
+    char grade = 'A';
+
+    // Boolean variable
+    bool isStudent = true;
+
+    // String variable
+    string name = "Moamen";
+
+    // Output
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "Height: " << height << endl;
+    cout << "Salary: " << salary << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Is Student: " << isStudent << endl;
+
+    return 0;
+}
